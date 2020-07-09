188b79b28faddd389d3f2bafd7f1b6c69cb73b58
Manish Thakur <45007295+Manish-Thakur@users.noreply.github.com>
2020-07-09 10:53:08 +0530

    Update inputoutput.py

--- inputoutput.py
+++ inputoutput.py
@@ -1,14 +1,14 @@
 #Program to write first program "HelloWorld" and demonstrate the user input/output operation
 
 print("Hello World!")
-print("---------------------------------")
+print("---------------------------------") #taking user inputs
 name=input("Enter your Name: ")
 age=int(input("Enter your Age: "))
 prof=input("Enter your profession: ")
 add=input("Enter your address: ")
 phone=int(input("Enter phone number: "))
 
-print("--------------Details-------------------")
+print("--------------Details-------------------") #displaying user details
 print("Name: ",name)
 print("Age: ",age)
 print("Profession: ",prof)
