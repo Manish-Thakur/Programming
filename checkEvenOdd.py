c98c647d1dd8c83dce7642fa7cc30282cb014559
Manish Thakur <manishkth19@gmail.com>
2020-07-09 20:23:33 +0530

    prog to check even and odd

---
+++ checkEvenOdd.py
@@ -0,0 +1,23 @@
+#program to check for even and odd
+print("------------Check-o-Meter---------------")
+print("Let's start")
+def check_EvenOdd(num):
+        if(num % 2==0):
+                print(num,"is Even")
+        else:
+                print(num,"is Odd")
+
+while True:
+    
+    def main():
+        x= int(input("Enter the number:"))
+        check_EvenOdd(x)
+    
+        
+    if __name__ == "__main__":
+         main()
+
+    if input("Continue...(y/n): ") !='y':
+        print("Thanks for Playing,Bye!")
+        break
+ 
