4f49b32fc7867f0abe28786807cdcafe9124d03e
Manish Thakur <manishkth19@gmail.com>
2020-07-13 00:33:42 +0530

    program to reverse a string

---
+++ reverseString.py
@@ -0,0 +1,29 @@
+#program to reverse a given string
+
+#Method1: using recursion.
+#-------------------------------#
+def reverse_string(s):
+    if len(s)==0:
+        return s
+    else:
+        return reverse_string(s[1:])+s[0]
+
+#Method2: using loop.
+#-------------------------------#
+def reverse_string(s):
+    str= ""
+    for i in s:
+        str=i+str
+
+    return str
+
+
+def Main():
+    string=input("Enter the string: ")
+    print("original string: ",string)
+    result=reverse_string(string)
+    print("reversed string: ",result)
+
+if __name__ == "__main__":
+    Main()
+    
