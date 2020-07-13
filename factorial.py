8c5c52054271034fd5a426a7f3f9c50315b7a0c2
Manish Thakur <manishkth19@gmail.com>
2020-07-13 21:09:38 +0530

    print factorial of a number

---
+++ factorial.py
@@ -0,0 +1,22 @@
+#program to print the factorial of a number
+
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def Main():
+    num=int(input("Enter the number: "))
+    result=factorial(num)
+    print("Factorial: ",result)
+
+if __name__ == "__main__":
+    Main()
+     
+#Output:
+#------------------#
+Enter the number: 5
+Factorial = 120
+
+
