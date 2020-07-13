79802ef58c9d2460d0f7688cf55dc8af519da4d9
Manish Thakur <manishkth19@gmail.com>
2020-07-13 19:59:45 +0530

    printing first N natural numbers

--- FirstNnaturalno.py
+++ FirstNnaturalno.py
@@ -2,13 +2,20 @@
 
 def first_N(num):
     sum=0
-    print("First "+str(num) +" Natural Numbers: \n")
-    for i in range(0,num):
+    print("First "+str(num) +" Natural Numbers: ")
+    for i in range(0,num,+1):
         sum+=1
         print(sum)
 
+#program to print N natural numbers in reverse order
 
-
+def first_N(num):
+    sum=num
+    print("First ", num , " Natural numbers in reverse: ")
+    for i in range(num,0,-1):
+        print(sum)
+        sum-=1
+        
 
 def main():
     n=int(input("Enter the limit: "))
