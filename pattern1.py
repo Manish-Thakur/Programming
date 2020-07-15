6f0844d1fdef8644999429734f5eb6f4e1a4241c
Manish Thakur <manishkth19@gmail.com>
2020-07-16 00:18:14 +0530

    pattern type 1

---
+++ pattern1.py
@@ -0,0 +1,21 @@
+#prograam to print a pattern
+
+n=int(input("Enter the limit: "))
+for i in range(1,n+1):
+    for j in range(0,i):
+        print("*",end='')
+    print('\n')
+
+
+#output
+#------------------#
+Enter the limit: 5
+*
+**
+***
+****
+*****
+        
+        
+    
+
